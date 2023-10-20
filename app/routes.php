3b1185c42767e2c25b8a04f9179b612eee4e2055
cmkqwerty <56502922+cmkqwerty@users.noreply.github.com>
2023-10-20 17:07:13 +0300

    [feat] router definition refactor

---
+++ app/routes.php
@@ -0,0 +1,13 @@
+<?php
+
+use Slim\App;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+return function (App $app) {
+    $app->get('/', function (Request $request, Response $response, $parameters) {
+        $response->getBody()->write('init');
+
+        return $response;
+    });
+};
--- bootstrap/index.php
+++ bootstrap/index.php
@@ -2,8 +2,6 @@
 
 use DI\Container;
 use Slim\Factory\AppFactory;
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -18,10 +16,7 @@ $app = AppFactory::create();
 $middleware = require __DIR__ . '/../app/middleware.php';
 $middleware($app);
 
-$app->get('/', function (Request $request, Response $response, $parameters) {
-    $response->getBody()->write('init');
-
-    return $response;
-});
+$routes = require __DIR__ . '/../app/routes.php';
+$routes($app);
 
 $app->run();
