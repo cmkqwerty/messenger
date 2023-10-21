1aaf6c12660c173104510e2a438684434ab8fa07
cmkqwerty <56502922+cmkqwerty@users.noreply.github.com>
2023-10-21 12:59:14 +0300

    [feat] add error whoops/middleware

--- app/Providers/ErrorMiddlewareServiceProvider.php
+++ app/Providers/ErrorMiddlewareServiceProvider.php
@@ -2,15 +2,15 @@
 
 namespace App\Providers;
 
+use Zeuxisoo\Whoops\Slim\WhoopsMiddleware;
+
 class ErrorMiddlewareServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app->addErrorMiddleware(
-            config('middleware.error_details.displayErrorDetails'),
-            config('middleware.error_details.logErrors'),
-            config('middleware.error_details.logErrorDetails')
-        );
+        if (env('APP_DEBUG') === true) {
+            $this->app->add(new WhoopsMiddleware());
+        }
     }
 
     public function boot()
