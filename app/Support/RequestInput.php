e67baa919f5da5d8b75a977f754577660648a155
cmkqwerty <56502922+cmkqwerty@users.noreply.github.com>
2023-10-23 13:38:15 +0300

    [feat] request input class

--- app/Support/RequestInput.php
+++ app/Support/RequestInput.php
@@ -24,4 +24,68 @@ class RequestInput
     {
         return $this->attributes;
     }
+
+    public function __set($property, $value)
+    {
+        $this->attributes[$property] = $value;
+    }
+
+    public function __get($property)
+    {
+        throw_when(!isset($this->attributes[$property]), "{$property} does not exist");
+
+        return $this->attributes[$property];
+    }
+
+    public function __invoke($property)
+    {
+        return data_get($this->attributes, $property);
+    }
+
+    public function forget($property)
+    {
+        unset($this->attributes[$property]);
+
+        return $this;
+    }
+
+    public function merge($array)
+    {
+        array_walk($array, fn ($value, $key) => data_set($this->attributes, $key, $value));
+
+        return $this;
+    }
+
+    public function fill($array)
+    {
+        array_walk($array, fn ($value, $key) => data_fill($this->attributes, $key, $value));
+
+        return $this;
+    }
+
+    // route meta informations
+    public function getCurrentUri()
+    {
+        return data_get($this->meta, 'currentUri');
+    }
+
+    public function getName()
+    {
+        return data_get($this->meta, 'name');
+    }
+
+    public function getGroups()
+    {
+        return data_get($this->meta, 'groups');
+    }
+
+    public function getMethods()
+    {
+        return data_get($this->meta, 'methods');
+    }
+
+    public function getArguments()
+    {
+        return data_get($this->meta, 'arguments');
+    }
 }
